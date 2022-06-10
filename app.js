af92e8298f5abcdc58558bd99bdf1d32c0cd05f1
Mario Vieira <mario@Marios-MacBook-Pro.local>
2022-06-10 23:00:33 +0000

    Code cleanup

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ const path = require('path')
 const AutoLoad = require('fastify-autoload')
 
 module.exports = async function (fastify, opts) {
-  // Place here your custom code!
-
-  // Do not touch the following lines
-
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
   // through your application
