c670aabc1a3c74be1c37bd69dc00c353a2695224
MarioWork <mariovieira.work@hotmail.com>
2022-06-19 11:08:51 +0000

    removed prisma plugin

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const fastifySensible = require('fastify-sensible');
 
 const api = require('./plugins/api');
-const prisma = require('./plugins/prisma');
 const shutdown = require('./plugins/shutdown');
 
 module.exports = async function (fastify, opts) {
   fastify
     .register(fastifySensible)
     .register(api)
-    .register(prisma)
     .register(shutdown)
 
 }
