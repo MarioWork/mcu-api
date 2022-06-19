cca9fd976973a257b1b92df21f538c9482077e9c
MarioWork <mariovieira.work@hotmail.com>
2022-06-19 16:12:52 +0000

    use case to generate key - in progress

---
+++ use-cases/generate-key.js
@@ -0,0 +1,19 @@
+const { v4: uuidv4 } = require('uuid');
+
+
+module.exports = async function (prisma) {
+    try {
+        const key = uuidv4();
+
+        await prisma.user.create({
+            data: {
+                key
+            }
+        })
+
+        return { key };
+    } catch (e) {
+        // throw new Error("Something went wrong generating key");
+        throw new Error(e.message);
+    }
+}
