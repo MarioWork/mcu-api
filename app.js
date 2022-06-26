099c70b1679455df60202c50bf15f6a2b520aa81
MarioWork <mariovieira.work@hotmail.com>
2022-06-26 18:01:25 +0000

    register the authentication plugin

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const fastifySensible = require('fastify-sensible');
 
+const authentication = require('./plugins/authentication');
 const api = require('./plugins/api');
 const prisma = require('./plugins/prisma');
 const shutdown = require('./plugins/shutdown');
 
 module.exports = async function (fastify, opts) {
   fastify
+    .register(authentication)
     .register(fastifySensible)
     .register(api)
     .register(prisma)
